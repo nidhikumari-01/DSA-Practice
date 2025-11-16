d739a20d7a78af4a14f31a78a6f3fd07d6cfedcc
Nidhi Kumari <nidhikumari140302@gmail.com>
2025-11-16 21:32:47 +0530

    Add basic array program to sum elements

--- FirstProgram/FirstProgram.js
+++ FirstProgram/FirstProgram.js
@@ -1,1 +1,12 @@
+// Basic Array Program: Sum of elements
+
+let numbers = [10, 20, 30, 40, 50]; // array
+
+let sum = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    sum = sum + numbers[i];       // adding each element
+}
+
+console.log("Sum of array elements is:", sum);
 
